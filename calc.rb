3d886f862b63e58310f7dab36065881f03047e2c
Litos <windsochi@gmail.com>
2014-08-07 01:54:09 +0400

    chomp destroy Enter for end string

--- calc.rb
+++ calc.rb
@@ -1,1 +1,13 @@
 puts 1 + 2
+puts 9.0 / 2
+i = 60*60*12*30*12
+puts "Секунд в году: "
+puts i
+puts 943000000 / 15552000
+puts 'Hello' + ' ' + 'world!'
+puts '5'.to_i * 5
+puts '\'Отлично!\''
+
+puts 'Ваше имя?'
+name = gets.chomp
+puts 'Вас зовут ' + name + '?. Вот и хорошо.'
